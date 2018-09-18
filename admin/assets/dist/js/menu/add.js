d2b451cd08343909825366b45668f25bb5d963f1
tunguyen <tunguyenhsgs@gmail.com>
2018-09-18 10:13:57 +0700

    18.09.18

--- admin/assets/dist/js/menu/add.js
+++ admin/assets/dist/js/menu/add.js
@@ -1,6 +1,7 @@
+var type = location.href.split('type=')[1].split('&')[0];
 $(document).ready(function () {
     $('.theform').submit(function () {
-        $.post(MAIN_URL+'/requests/menu/add.php', $(this).serialize(), function (data) {
+        $.post(MAIN_URL+'/requests/menu/add.php?type='+type, $(this).serialize(), function (data) {
             console.log(data);
             if (data == 1) location.href = MAIN_URL+'/menu';
         })
