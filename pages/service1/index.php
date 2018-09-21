86e655443e72ff4aee9d3aa7b7adb5c370c5f018
tunguyen <tunguyenhsgs@gmail.com>
2018-09-21 21:55:12 +0700

    21.09.18

--- pages/service1/index.php
+++ pages/service1/index.php
@@ -1,20 +1,27 @@
-<h2 class="module-title"><span>Xét nghiệm ung thư</span></h2>
+<h2 class="module-title"><span>Tin tức</span></h2>
 <div class="category-product">
     <div class="home-list-news">
-        <?php foreach ($newsList as $nO) { ?>
+        <?php foreach ($newsList as $nO) {?>
         <div class="home-news-item cat-news-item">
             <div>
                 <a href="<?php echo $nO['link'] ?>">
                     <img src="<?php echo $nO['image'] ?>" alt="<?php echo $nO['title'] ?>">
                 </a>
-                <h3><a href="<?php echo $nO['link'] ?>">
-                        <?php echo $nO['title'] ?></a>
+                <h3>
+                    <a href="<?php echo $nO['link'] ?>">
+                        <?php echo $nO['title'] ?>
+                    </a>
                 </h3>
-                <div class="news-price">
-                    Giá: <span><?php echo $nO['price'] ?></span>
-                </div>
+                <p>
+                <?php if ($tvN['highlight_content']) {
+                    echo substr(remove_bbcode($nO['highlight_content']), 0, 500);
+                } else {
+                    echo substr(remove_bbcode($nO['content']), 0, 500);
+                }?>... 
+                <a href="<?php echo $nO['link'] ?>" class="readmore-1">Xem thêm</a>
+                </p>
             </div>
         </div>
-        <?php } ?>
+        <?php }?>
     </div>
 </div>
